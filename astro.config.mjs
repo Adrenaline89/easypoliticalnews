f0d8c6eb78bd770bf886b2adb460269bd11f8d62
Andrenaline89 <paul.forwardca@gmail.com>
2025-02-28 22:11:19 -0800

    add space

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
