ff3acd4981f6a925ba70981f0bd1bda68e44acff
Andrenaline89 <paul.forwardca@gmail.com>
2025-02-28 16:31:59 -0800

    readd tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
-  output: 'hybrid'
-});
+  integrations: [react(), markdoc(), tailwind(), ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()])],
+  output: (process.env.SKIP_KEYSTATIC ? 'static' : 'hybrid')
+}); 
