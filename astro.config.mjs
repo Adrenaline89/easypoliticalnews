dfc46e263888acacde04467fe1bd445199c6b2e3
Andrenaline89 <paul.forwardca@gmail.com>
2025-02-28 17:50:44 -0800

    remove env file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
 import tailwind from "@astrojs/tailwind";
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
